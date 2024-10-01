0277313eca37ac918190d0ec3f27107842ed3ddb
Mihai Surdeanu <contact@mihaisurdeanu.ro>
2024-10-01 20:30:19 +0300

    Implement more tests

---
+++ src/test/java/org/myworkflows/domain/command/GroovyCommandTest.java
@@ -0,0 +1,74 @@
+package org.myworkflows.domain.command;
+
+import org.junit.jupiter.api.Test;
+import org.myworkflows.domain.ExpressionNameValue;
+import org.myworkflows.domain.RuntimeEvaluator;
+import org.myworkflows.domain.WorkflowRun;
+import org.myworkflows.exception.WorkflowRuntimeException;
+
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * @author Mihai Surdeanu
+ * @since 1.0.0
+ */
+public final class GroovyCommandTest {
+
+    @Test
+    public void whenSimpleGroovyCodeIsRun_thenEverythingWorksAsExpected() {
+        // given
+        final var workflowRun = new WorkflowRun();
+        workflowRun.getCache().put("scriptLines", List.of(
+            "import org.myworkflows.domain.WorkflowRunCache",
+            "def String run(WorkflowRunCache cache) {",
+            "  return \"Test\"",
+            "}"
+        ));
+
+        // when & then
+        assertDoesNotThrow(() -> new GroovyCommand("A", Set.of(), Set.of(), Set.of(),
+            Set.of(new ExpressionNameValue("var", "#output", RuntimeEvaluator.SPEL))).run(workflowRun));
+        assertEquals("Test", workflowRun.getCache().get("var", String.class));
+    }
+
+    @Test
+    public void whenSimpleGroovyCodeIsRunWithOptionalParams_thenEverythingWorksAsExpected() {
+        // given
+        final var workflowRun = new WorkflowRun();
+        workflowRun.getCache().put("scriptLines", List.of(
+            "import org.myworkflows.domain.WorkflowRunCache",
+            "def int myRun(WorkflowRunCache cache) {",
+            "  return 1",
+            "}"
+        ));
+        workflowRun.getCache().put("methodName", "myRun");
+
+        // when & then
+        assertDoesNotThrow(() -> new GroovyCommand("A", Set.of(), Set.of(), Set.of(),
+            Set.of(new ExpressionNameValue("var", "#output", RuntimeEvaluator.SPEL))).run(workflowRun));
+        assertEquals(1, workflowRun.getCache().get("var", Integer.class));
+    }
+
+    @Test
+    public void whenSimpleGroovyCodeIsRunWithSyntaxIssue_thenAProperExceptionIsRaised() {
+        // given
+        final var workflowRun = new WorkflowRun();
+        workflowRun.getCache().put("scriptLines", List.of(
+            "import org.myworkflows.domain.WorkflowRunCache",
+            "def int myRun(WorkflowRunCache cache) {",
+            "  return 1",
+            "" // we forgot "}"
+        ));
+        workflowRun.getCache().put("methodName", "myRun");
+
+        // when & then
+        assertThrows(WorkflowRuntimeException.class,
+            () -> new JavaCommand("A", Set.of(), Set.of(), Set.of(), Set.of()).run(workflowRun));
+    }
+
+}
